e9111d116ebcb64dc98131846a7a8e6492a4d2d6
Мария Мелентьева <masha696nik@gmail.com>
2025-08-17 15:46:17 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return <h1 className=" text-blue-600   font-bold  "> Hello </h1>
+}
+export default App
