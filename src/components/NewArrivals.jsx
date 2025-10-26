f126b11cc695a4dc71dd2b6a557d9f5fda0b98cf
Мария Мелентьева <masha696nik@gmail.com>
2025-10-26 23:25:58 +0500

    refactor: switching to the redux store

--- src/components/NewArrivals.jsx
+++ src/components/NewArrivals.jsx
@@ -2,47 +2,51 @@ import React from 'react'
 import ProductCard from './ProductCard'
 import useScrollAnimation from '../hooks/useScrollAnimation'
 import { Link } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
 
 const NewArrivals = () => {
   useScrollAnimation()
-  let products = [
-    {
-      id: 1,
-      imgPath: './img/products/black-t-shirt.png',
-      title: 'T-shirt With Tape Details',
-      rating: 4.5,
-      price: 120,
-    },
-    {
-      id: 2,
-      imgPath: './img/products/blue-jeans.png',
-      title: 'Skinny Fit Jeans',
-      rating: 3.5,
-      price: 260,
-      sale: 20,
-    },
-    {
-      id: 3,
-      imgPath: './img/products/checkered-shirt.png',
-      title: 'Checkered Shirt',
-      rating: 4.5,
-      price: 180,
-    },
-    {
-      id: 4,
-      imgPath: './img/products/stripped-t-shirt.png',
-      title: 'Sleeve Striped T-shirt',
-      rating: 4.5,
-      price: 160,
-      sale: 30,
-    },
-  ]
+  const products = useSelector((state) => state.product.products)
+  const newProducts = products.filter((product) => product.isNew)
+  newProducts.sort(() => 0.5 - Math.random())
+  // let products = [
+  //   {
+  //     id: 1,
+  //     imgPath: './img/products/black-t-shirt.png',
+  //     title: 'T-shirt With Tape Details',
+  //     rating: 4.5,
+  //     price: 120,
+  //   },
+  //   {
+  //     id: 2,
+  //     imgPath: './img/products/blue-jeans.png',
+  //     title: 'Skinny Fit Jeans',
+  //     rating: 3.5,
+  //     price: 260,
+  //     sale: 20,
+  //   },
+  //   {
+  //     id: 3,
+  //     imgPath: './img/products/checkered-shirt.png',
+  //     title: 'Checkered Shirt',
+  //     rating: 4.5,
+  //     price: 180,
+  //   },
+  //   {
+  //     id: 4,
+  //     imgPath: './img/products/stripped-t-shirt.png',
+  //     title: 'Sleeve Striped T-shirt',
+  //     rating: 4.5,
+  //     price: 160,
+  //     sale: 30,
+  //   },
+  // ]
 
   return (
     <section className="scroll-hidden mx-auto flex max-w-7xl flex-col items-center gap-13 border-b-1 border-black/10 pt-18 pb-16 sm:gap-8 sm:pt-12 sm:pb-10 xl:container">
       <h2 className="title title-section text-5xl">NEW ARRIVALS</h2>
       <div className="no-scrollbar grid grid-cols-4 gap-5 overflow-auto max-xl:flex max-xl:w-full max-xl:max-w-full max-xl:pl-5 max-sm:[&>*]:min-w-[250px] sm:[&>*]:min-w-[300px]">
-        {products.map((product) => {
+        {newProducts.slice(0, 4).map((product) => {
           return (
             <div className="" key={product.id}>
               <ProductCard product={product} />
--- src/components/TopSelling.jsx
+++ src/components/TopSelling.jsx
@@ -2,46 +2,20 @@ import React from 'react'
 import ProductCard from './ProductCard'
 import useScrollAnimation from '../hooks/useScrollAnimation'
 import { Link } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
 
 const TopSelling = () => {
   useScrollAnimation()
-  let products = [
-    {
-      id: 1,
-      imgPath: './img/products/stripped-shirt.png',
-      title: 'Sleeve Striped T-shirt',
-      rating: 5.0,
-      price: 212,
-      sale: 20,
-    },
-    {
-      id: 2,
-      imgPath: './img/products/orange-t-shirt.png',
-      title: 'Courage Graphic T-shirt',
-      rating: 4.0,
-      price: 145,
-    },
-    {
-      id: 3,
-      imgPath: './img/products/bermuda-shorts.png',
-      title: 'Loose Fit Bermuda Shorts',
-      rating: 3.0,
-      price: 80,
-    },
-    {
-      id: 4,
-      imgPath: './img/products/black-jeans.png',
-      title: 'Faded Skinny Jeans',
-      rating: 4.5,
-      price: 210,
-    },
-  ]
+
+  const products = useSelector((state) => state.product.products)
+  const topProducts = products.filter((product) => product.isTop)
+  topProducts.sort(() => 0.5 - Math.random())
 
   return (
     <section className="scroll-hidden mx-auto flex max-w-7xl flex-col items-center gap-13 pt-16 pb-20 sm:gap-8 sm:pt-12 sm:pb-10 xl:container">
       <h2 className="title title-section text-5xl">TOP SELLING</h2>
       <div className="no-scrollbar grid grid-cols-4 gap-5 overflow-auto max-xl:flex max-xl:w-full max-xl:max-w-full max-xl:pl-5 max-sm:[&>*]:min-w-[250px] sm:[&>*]:min-w-[300px]">
-        {products.map((product) => {
+        {topProducts.slice(0, 4).map((product) => {
           return (
             <div className="" key={product.id}>
               <ProductCard product={product} />
